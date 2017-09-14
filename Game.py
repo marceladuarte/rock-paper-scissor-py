7126066786b883d69dc0d3d832b046f61fc036aa
marceladuarte <marcela.pedrinid@gmail.com>
2017-09-13 22:55:42 -0300

    getting user input

---
+++ Game.py
@@ -0,0 +1,14 @@
+class Game:
+
+    def __init__(self):
+        pass
+
+
+    def play(self):
+        print("Let's play rock paper or scissors")
+        print("Choose one option: (1) Rock (2) Paper (3) Scissors")
+        answer = input()
+        print("Your answer: " + answer)
+
+
+Game().play()
